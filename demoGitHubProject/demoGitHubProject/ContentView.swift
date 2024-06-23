03e44fa8fe04b67a141d6d11a17fa848623ca12d
ynp16 <y.nphuensaen@gmail.com>
2024-06-23 10:07:19 +0700

    add files

---
+++ demoGitHubProject/demoGitHubProject/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  demoGitHubProject
+//
+//  Created by Naliya Phuensaen on 23/6/2567 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ demoGitHubProject/demoGitHubProject/demoGitHubProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  demoGitHubProjectApp.swift
+//  demoGitHubProject
+//
+//  Created by Naliya Phuensaen on 23/6/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct demoGitHubProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
